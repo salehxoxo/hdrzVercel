107156ed6119776e8be9dcd508320a3d7a35159b
saleh <salehshamoon52@gmail.com>
2024-10-28 10:41:10 +0500

    11

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { load } from "cheerio";
 const app = express();
 // const PORT = 3000;
 
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
 // Define your getId function here
 const getId = async (req, res) => {
